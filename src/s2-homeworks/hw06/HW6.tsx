ceec6517c9ca8908498117e46109a0ebbc675fb0
zheton90 <jen.nikolenko@yandex.ru>
2025-07-23 19:03:36 +0300

    hw 6, first variant

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,7 +18,7 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        // делают студенты
+        setValue(restoreState<string>('hw6-editable-span-value', 'enter value'))
 
     }
 
